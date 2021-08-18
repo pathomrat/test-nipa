f666b3960b935dd48bc7b9d771a7abfec8743993
pathomrat <pathomrat97@gmail.com>
2021-08-18 20:24:51 +0700

    install express

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3001;
+
+app.route('').get((req, res) => {
+    res.send('Hello from Node.js RESTful API');
+});
+
+app.listen(port, () => {
+    console.log('server is running on port: ', port);
+});
