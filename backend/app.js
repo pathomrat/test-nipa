b20aea0b9b8d96e972641b041ba8bea2ec426f56
pathomrat <pathomrat97@gmail.com>
2021-08-19 00:27:34 +0700

    Add mongoose and dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
-
 const app = express();
 
 const port = 3001;
+
+//mongoDB connect
+var mongoose = require('mongoose');
+require('dotenv').config()
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+});
+mongoose.Promise = global.Promise;
 
 var indexApi = require('./routes/indexRoutes');
 var ticketApi = require('./routes/ticketRoutes');
---
+++ backend/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBylG3Evr_0E0TDnCNq1Rd3iOywwadllFY",
+    authDomain: "test-nipa.firebaseapp.com",
+    databaseURL: "https://test-nipa-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "test-nipa",
+    storageBucket: "test-nipa.appspot.com",
+    messagingSenderId: "516987103824",
+    appId: "1:516987103824:web:4fa5c2edcc1da98df8ff55"
+};
+
+export default firebaseConfig;
